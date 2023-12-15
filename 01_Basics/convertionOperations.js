91c1cf616cac379644a7fe78c2f328ca7ca8c8c4
Mel.b <68156268+melbinjacob@users.noreply.github.com>
2023-12-15 15:36:10 +0530

    Completed Convertion operations

---
+++ 01_Basics/convertionOperations.js
@@ -0,0 +1,38 @@
+// let score = "33ab";
+let score = null;
+
+// console.log(typeof(score));
+
+let valueInNumber = Number(score)
+
+// console.log(typeof(valueInNumber));
+// console.log(valueInNumber);
+
+
+// CONVERTION using Number()
+
+// "33" => 33 (Number)
+// "33abc" => NaN (Not a number)
+// True (Bool) => 1 (Number)
+// False (Bool) => 0 (Number)
+// False (Bool) => 0 (Number)
+
+
+// let isLoggedIn = 1;
+// let isLoggedIn = "";
+let isLoggedIn = "j";
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+// console.log(booleanIsLoggedIn);
+
+// 1 => True
+// 0 => False
+// "" => False
+// "Stiring" => True
+
+let someNumber = 33;
+let stringNumber = String(someNumber)
+
+console.log(stringNumber);
+console.log(typeof(stringNumber));
