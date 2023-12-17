3bdfeafb259caa66fcd9984174d11192b26228f8
Mel.b <68156268+melbinjacob@users.noreply.github.com>
2023-12-17 17:19:22 +0530

    Comparison operators

--- 01_Basics/convertionOperations.js
+++ 01_Basics/convertionOperations.js
@@ -34,5 +34,60 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 let someNumber = 33;
 let stringNumber = String(someNumber)
 
-console.log(stringNumber);
-console.log(typeof(stringNumber));
+// console.log(stringNumber);
+// console.log(typeof(stringNumber));
+
+// *******************************************OPERATIONS*********************************************
+
+let value = 3
+let negValue = -value
+
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2); // 2 Raised to the power of 2
+// console.log(2**3); // 2 Raised to the power of 3 
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = "Hello"
+let str2 = " Melbin"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log(1 + 1 + "2");
+// console.log("1" + 1 + 2);
+
+
+// console.log(true);
+// console.log(+true);
+// console.log(+"");
+
+
+// *******************************************PREFIX AND POSTFIX**************************************
+
+
+let gameCounter = 100;
+gameCounter++
+console.log(gameCounter);
+
+++gameCounter
+console.log(gameCounter);
+
+
+let x = 3;
+const y = x++;
+
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+let a = 3;
+const b = ++a;
+
+console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4"
