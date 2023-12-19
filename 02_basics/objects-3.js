674da4b13bc298916bee42b1050d519307cfb370
Mel.b <68156268+melbinjacob@users.noreply.github.com>
2023-12-19 11:19:01 +0530

    jsonTalk

---
+++ 02_basics/objects-3.js
@@ -0,0 +1,24 @@
+const course = {
+    courseName : "Js in hindi",
+    price : "999",
+    courseInstructor: "Hithesh"
+}
+
+// course.courseInstructor
+
+const {courseInstructor : instructur} = course;
+
+console.log(instructur);
+
+
+const navbar = () => {
+    
+}
+
+navbar(company = "Hitesh")
+
+// {
+//     "Name" : "Melbin",
+//     "Age" : 12,
+//     "Place": "Kasargod"
+// }
